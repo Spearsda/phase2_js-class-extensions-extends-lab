ee2f91af1cfab0161b8141ae06fdbf8a71586ab1
Damian <spearsda16@gmail.com>
2021-05-08 20:34:21 -0400

    everything but the validity

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+
+    get countSides() {
+        return this.array.length 
+    }
+
+    get perimeter() {
+        return this.array.reduce((a, b) => a + b, 0)
+    }
+
+}
+
+
+class Triangle extends Polygon {
+
+    get isValid() {
+    
+    }
+
+}
+
+
+class Square extends Polygon {
+
+
+    get isValid() {
+        
+
+    }
+
+    get area() {
+        return this.array[0] * this.array[1]
+    }
+}
